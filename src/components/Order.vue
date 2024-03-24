4aab2c34c2fb0addce614245d99ad741502576de
Jean-Yves Lalanne <jylalanne@demboost.com>
2024-03-24 17:50:15 +0100

    DEV Parcel route with Order component

---
+++ src/components/Order.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ order.id }}</h1>
+    <div>{{ order.date }}</div>
+    <ul>
+      <li v-for="item in order.items" :key="item.id">
+        {{ item.id }} : {{ item.quantity }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component'
+
+  @Options({
+    props: {
+      order: {
+        id: String,
+        date: Date,
+        items: Array,
+      },
+    },
+  })
+  export default class Order extends Vue {
+    public order!: {
+      id: string
+      date: Date
+      items: { id: string; quantity: number }[]
+    }
+  }
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/ParcelsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>Parcel view</h1>
+    <Order v-for="order in orders" :key="order.id" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component'
+  import Order from '@/components/Order.vue'
+
+  @Options({
+    components: {
+      Order,
+    },
+  })
+  export default class HomeView extends Vue {
+    private orders = [
+      {
+        id: 'someID',
+        date: new Date(),
+        items: [{ id: 'itemID', quantity: 4 }],
+      },
+    ]
+  }
+</script>
